cec89beb5fb33d30dd9af37a7a9ba5008bd304d9
AOTech <86697379+aotech7@users.noreply.github.com>
2023-12-23 18:39:14 -0800

    Add files via upload

---
+++ tkinter.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+
+#here you are importing the class TK (remember classes start with a capital letter)
+
+root = Tk()
+root.title("Welcome to the Privacy Corner Window")
+
+
+w = Label(root, text="This is Privacy Hub, a site for learning basic privacy concepts")
+
+w.pack()
+
+#keeps window open until user exits the window
+
+
+root.mainloop()
+          
